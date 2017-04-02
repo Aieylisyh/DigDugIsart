b296a6b2c9f80675e4abaf81f0ec2eb621dc688c
Dimitri <dimitri.pomier@gmail.com>
2017-04-02 14:12:01 +0200

    up

--- DigDug/Assets/Scripts/Object/Pick.cs
+++ DigDug/Assets/Scripts/Object/Pick.cs
@@ -13,16 +13,15 @@ public class Pick : MonoBehaviour {
         m_box = GetComponent<BoxCollider>();
         m_t = transform;
         m_gap = -transform.position.x;
+
+        if (m_world == null) Debug.LogWarning("Can not dig without World instance");
     }
 
     void Update () {
-       Dig( m_world );
+       if (m_world != null) Dig( m_world );
     }
 
     void Dig (MeshCreator world) {
-
-        
-
 
         world.RemoveBlock( new Vector2[4] {
             new Vector2( (m_t.position.x - m_box.bounds.extents.x) + m_gap, (m_t.position.y - m_box.bounds.extents.y) + m_gap ),
