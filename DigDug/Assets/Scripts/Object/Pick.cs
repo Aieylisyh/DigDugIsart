76636c1a209653b9393ee9051bce7c266f1f2f4f
Dimitri <dimitri.pomier@gmail.com>
2017-04-02 13:49:17 +0200

    dig action

---
+++ DigDug/Assets/Scripts/Object/Pick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pick : MonoBehaviour {
+
+    public MeshCreator m_world;
+
+    private Transform m_t;
+    private BoxCollider m_box;
+    private float m_gap;
+
+    void Awake () {
+        m_box = GetComponent<BoxCollider>();
+        m_t = transform;
+        m_gap = -transform.position.x;
+    }
+
+    void Update () {
+       Dig( m_world );
+    }
+
+    void Dig (MeshCreator world) {
+
+        
+
+
+        world.RemoveBlock( new Vector2[4] {
+            new Vector2( (m_t.position.x - m_box.bounds.extents.x) + m_gap, (m_t.position.y - m_box.bounds.extents.y) + m_gap ),
+            new Vector2( (m_t.position.x - m_box.bounds.extents.x) + m_gap, (m_t.position.y + m_box.bounds.extents.y) + m_gap ),
+            new Vector2( (m_t.position.x + m_box.bounds.extents.x) + m_gap, (m_t.position.y - m_box.bounds.extents.y) + m_gap ),
+            new Vector2( (m_t.position.x + m_box.bounds.extents.x) + m_gap, (m_t.position.y + m_box.bounds.extents.y) + m_gap )
+        } );
+    }
+}
