0c31fe109285b27526f9755f67c673d6e9504427
Dimitri <dimitri.pomier@gmail.com>
2017-04-02 15:49:39 +0200

    ui: title card, scores

---
+++ DigDug/Assets/Scripts/UI/ScoresViewer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoresViewer : MonoBehaviour {
+
+    public HorizontalOrVerticalLayoutGroup ScoreLayout;
+    
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DigDug/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager instance;
+
+    public string GameSceneName = "Game";
+    public Canvas ScoreMenu;
+    public Canvas TitleCardMenu;
+
+    Canvas m_opened;
+
+    void Awake () {
+        if (instance == null) instance = this;
+        else Destroy(this.gameObject);
+    }
+
+    public void StartGame () {
+        SceneManager.LoadScene( GameSceneName );
+    }
+
+    public void OpenScores () {
+        CloseMenu();
+        m_opened = Instantiate( ScoreMenu );
+    }
+
+    public void OpenTitleCard () {
+        CloseMenu();
+        m_opened = Instantiate( TitleCardMenu );
+    }
+
+    public void CloseMenu () {
+        if (m_opened != null && m_opened.gameObject != null) Destroy( m_opened.gameObject );
+    }
+}
